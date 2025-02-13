1bc28d951bad9c4a9fb117ff83ee4e490c898863
Justin Naylor <justin.n@carketa.com>
2025-02-13 02:32:57 -0700

    Added prisma fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,13 +4,11 @@ const prisma = new PrismaClient({
   datasources: {
     db: {
       url:
-        process.env.NODE_ENV !== "production"
+        process.env.NODE_ENV === "production"
           ? process.env.DATABASE_URL_PROD
           : process.env.DATABASE_URL,
     },
   },
 })
-
-console.log("env", prisma)
 
 export default prisma
